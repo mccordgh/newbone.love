73f5f83cf261b2adfc5e24d54e336ac83b038bf0
Matthew McCord <mattm.eternal@gmail.com>
2019-06-02 13:57:05 -0700

    Make modules global objects for easier access

--- main.lua
+++ main.lua
@@ -15,9 +15,9 @@
 -- iPhone 6s	        750px × 1334px	        1334px × 750px
 -- iPhone SE	        640px × 1136px	        1136px × 640px
 
-local game = require("game")
-local player = require("player")
-local game_board = require("game_board")
+require("game")
+require("player")
+require("game_board")
 
 function love.load()
     game.init()
--- player.lua
+++ player.lua
@@ -1,10 +1,9 @@
-local player = {}
+player = {}
 
 function player.init()
     player.height = 48
     player.width = 32
     player.x = (love.graphics.getWidth() / 2) - (player.width / 2)
     player.y = love.graphics.getHeight() - player.height
+    player.speed = 200
 end
-
-return player
