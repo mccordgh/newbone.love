6144633459fe869ac3fa5c552698ad80a4cae461
Matthew McCord <mattm.eternal@gmail.com>
2019-06-02 13:55:46 -0700

    Logic for drawing the game board tiles

--- game_board.lua
+++ game_board.lua
@@ -1,14 +1,16 @@
-local game_board = {}
+game_board = {}
 
-function game_board.draw_board(screen_height, screen_width, tile_height, tile_width)
-    love.graphics.print("screen_height: " .. screen_height, 10, 0)
-    love.graphics.print("screen_width: " .. screen_width, 10, 20)
-    love.graphics.print("tile_height: " .. tile_height, 10, 40)
-    love.graphics.print("tile_width: " .. tile_width, 10, 60)
-    -- for x = 0, screen_width, tile_width
-    -- do
-    --     print(x)
-    -- end
+function game_board.draw_board()
+    for x = 0, game.vertical_tile_count - 1, 1
+    do
+        for y = 0, game.horizontal_tile_count - 1, 1
+        do
+            love.graphics.setColor(255, 255, 255, 1)
+            -- love.graphics.rectangle("line", x * game.tile_width, y * game.tile_height, game.tile_width, game.tile_height)
+
+            love.graphics.setColor(0, 255, 0, 1)
+            love.graphics.print("tile size = w: " .. game.tile_width .. ", h: " .. game.tile_height)
+        end
+    end
+
 end
-
-return game_board
