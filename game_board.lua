39601b9410ab014458f51c2d1471b5de0a451c1b
Matthew McCord <mattm.eternal@gmail.com>
2019-06-02 15:45:09 -0700

    Cleanup comments

--- game_board.lua
+++ game_board.lua
@@ -6,7 +6,6 @@ function game_board.init()
 end
 
 function game_board.update(dt)
-    -- local new_x = player.x + (player.horizontal_speed * x_dir * dt)
     game_board.vertical_scroll_offset = game_board.vertical_scroll_offset - (player.vertical_speed * dt)
 
     if game_board.vertical_scroll_offset <= -game.tile_height then
@@ -27,10 +26,6 @@ function game_board.draw_self()
 
             love.graphics.setColor(0, 0, 0, 1)
             love.graphics.rectangle("line", xx, yy , game.tile_width, game.tile_height)
-
-            -- love.graphics.setColor(0, 255, 0, 1)
-            -- love.graphics.print("tile size = w: " .. game.tile_width .. ", h: " .. game.tile_height)
-            -- love.graphics.print("vertical offset: " .. game_board.vertical_scroll_offset)
         end
     end
 
