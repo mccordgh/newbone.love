88347b4f947c585ca2c6b2c674391531deb6a22f
Matthew McCord <mattm.eternal@gmail.com>
2019-06-01 22:15:38 -0700

    Hello world\!

---
+++ main.lua
@@ -0,0 +1,20 @@
+-- DEVICE	            PORTRAIT DIMENSIONS	    LANDSCAPE DIMENSIONS
+-- 12.9″ iPad Pro	    2048px × 2732px	        2732px × 2048px
+-- 10.5″ iPad Pro   	1668px × 2224px	        2224px × 1668px
+-- 9.7″ iPad    	    1536px × 2048px	        2048px × 1536px
+-- 7.9″ iPad mini 4	    1536px × 2048px	        2048px × 1536px
+-- iPhone XS Max    	1242px × 2688px	        2688px × 1242px
+-- iPhone XS        	1125px × 2436px	        2436px × 1125px
+-- iPhone XR	        828px × 1792px	        1792px × 828px
+-- iPhone X	            1125px × 2436px	        2436px × 1125px
+-- iPhone 8 Plus	    1242px × 2208px	        2208px × 1242px
+-- iPhone 8	            750px × 1334px	        1334px × 750px
+-- iPhone 7 Plus    	1242px × 2208px	        2208px × 1242px
+-- iPhone 7	            750px × 1334px	        1334px × 750px
+-- iPhone 6s Plus   	1242px × 2208px	        2208px × 1242px
+-- iPhone 6s	        750px × 1334px	        1334px × 750px
+-- iPhone SE	        640px × 1136px	        1136px × 640px
+
+function love.draw()
+    love.graphics.print("Hello World!!!", 400, 300)
+end
